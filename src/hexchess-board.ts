05cec5e42061498ebf1b232e65bfeae4a003de29
Chander Ramesh <chander@usemotion.com>
2023-12-26 17:35:54 -0800

    Move CSS styles to a separate file

--- src/hexchess-board.ts
+++ src/hexchess-board.ts
@@ -24,6 +24,7 @@ import {
   fenToBoard,
 } from './utils';
 import {renderPiece} from './piece';
+import { styles } from './hexchess-styles';
 
 /**
  * A hexagonal chess board used for playing Glinsky-style hex chess.
@@ -41,67 +42,7 @@ import {renderPiece} from './piece';
  */
 @customElement('hexchess-board')
 export class HexchessBoard extends LitElement {
-  static override styles = css`
-    .cursor-grab {
-      cursor: grab;
-    }
-
-    .cursor-grabbing {
-      cursor: grabbing;
-    }
-
-    .piece {
-      fill: var(--hexchess-piece-bg, #000000);
-      pointer-events: none;
-    }
-
-    .possible-move > polygon {
-      fill: var(--hexchess-possible-move-bg, #a68a2d88);
-    }
-
-    .label {
-      fill: var(--hexchess-label-bg, #ffffff);
-      font-size: var(--hexchess-label-size, 12px);
-      /* Disable user selection */
-      /* https://stackoverflow.com/questions/826782/how-to-disable-text-selection-highlighting */
-      -webkit-touch-callout: none; /* iOS Safari */
-      -webkit-user-select: none; /* Safari */
-      -khtml-user-select: none; /* Konqueror HTML */
-      -moz-user-select: none; /* Old versions of Firefox */
-      -ms-user-select: none; /* Internet Explorer/Edge */
-      user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
-    }
-
-    .white {
-      fill: var(--hexchess-white-bg, #0fafdb);
-    }
-    .selected .white {
-      fill: var(--hexchess-selected-white-bg, #a68a2d);
-    }
-
-    .black {
-      fill: var(--hexchess-black-bg, #2a5966);
-    }
-    .selected .black {
-      fill: var(--hexchess-selected-black-bg, #a68a2d);
-    }
-
-    .grey {
-      fill: var(--hexchess-grey-bg, #24829c);
-    }
-    .selected .grey {
-      fill: var(--hexchess-selected-grey-bg, #a68a2d);
-    }
-
-    .possible-move {
-      fill: var(--hexchess-possible-move-bg, #a68a2d);
-    }
-
-    .drag-piece {
-      position: absolute;
-      z-index: 10;
-    }
-  `;
+  static override styles = styles;
 
   private _currentMove = -1;
   private _currentPosition: Board = {};
