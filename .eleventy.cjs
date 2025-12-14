48dba879fba402874fc6b0f031f68827cea79e78
Chander Ramesh <zomgtacoliscious@gmail.com>
2025-12-14 16:12:30 -0500

    Serve default audio files properly (#67)

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -8,6 +8,7 @@ module.exports = function (eleventyConfig) {
     'node_modules/@webcomponents/webcomponentsjs'
   );
   eleventyConfig.addPassthroughCopy('node_modules/lit/polyfill-support.js');
+  eleventyConfig.addPassthroughCopy('docs-src/assets');
   return {
     dir: {
       input: 'docs-src',
